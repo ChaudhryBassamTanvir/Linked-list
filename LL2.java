c6cf89d5331e808644fe0225070574817ded2358
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-11-04 22:32:04 +0500

    Updated

--- LL2.java
+++ LL2.java
@@ -1,5 +1,19 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class LL2 {
     public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
         
+Scanner sc = new Scanner(System.in);
+
+for (int i = 0; i < 5; i++) {
+list.add(sc.next());
+    
+}
+
+
+System.out.println(list);
+
     }
 }
