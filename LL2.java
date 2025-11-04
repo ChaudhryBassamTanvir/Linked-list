5d6843b86a01b468fc43e7a2fb0fabde81b9aff1
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-11-04 22:37:17 +0500

    Updated

--- LL2.java
+++ LL2.java
@@ -4,16 +4,39 @@ import java.util.Scanner;
 public class LL2 {
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<>();
+        LinkedList<String> list1 = new LinkedList<>();
         
 Scanner sc = new Scanner(System.in);
 
-for (int i = 0; i < 5; i++) {
-list.add(sc.next());
+// for (int i = 0; i < 5; i++) {
+// list.add(sc.next());
     
+// }
+
+
+// System.out.println(list);
+
+
+
+list1.addFirst("Bani");
+list1.addFirst("Sidhu");
+list1.addLast("b");
+list1.add("a list");
+System.out.println(list1);
+System.out.println(list1.size());
+
+
+
+
+for (int i = 0; i < list1.size(); i++) {
+    System.out.print(list1.get(i)+" -> ");
 }
+System.out.println("Null");
 
 
-System.out.println(list);
-
+list1.removeFirst();
+System.out.println(list1);
+list1.removeLast();
+System.out.println(list1);
     }
 }
