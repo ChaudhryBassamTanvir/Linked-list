1a65cebfb48b9cfa0d48e494056ab3b5f13ad89a
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-11-08 22:56:58 +0500

    Updated

--- PracticeLL.java
+++ PracticeLL.java
@@ -1,5 +1,20 @@
+import java.util.LinkedList;
+
 public class PracticeLL {
     public static void main(String[] args) {
-        
+        LinkedList<Integer> list = new LinkedList<>();
+list.add(1);
+list.add(2);
+list.add(3);
+list.add(4);
+list.add(5);
+
+
+for (int i = 0; i < list.size(); i++) {
+    
+}
+
+
+
     }
 }
