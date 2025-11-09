7c0918baaca465304340d2dfb2d97fd6e8bd08dd
Chaudhry Bassam Tanvir <chbassamtanvir@gmail.com>
2025-11-09 20:54:35 +0500

    Updated

--- PracticeLL.java
+++ PracticeLL.java
@@ -1,123 +1,133 @@
-import java.util.LinkedList;
-
 public class PracticeLL {
     Node head;
-private int size;
+    private int size;
 
-PracticeLL(){
-    this.size=0;
-}
-class Node{
-    int data;
-    Node next; // next b node ho ge tu 
+    PracticeLL() {
+        this.size = 0;
+    }
 
-   
-    Node(int data ){
+    class Node {
+        int data;
+        Node next;
 
+        Node(int data) {
             this.data = data;
-            this.next=null;
-             size=size+1;
-
-    }
-}
-
-//add ,first, last
-
-public void addFirst (int data){
-    
-    Node  newNode = new Node(data);
-    
-    if (head==null) {
-    head= newNode;
-    return;
-}
- newNode.next = head;
- head=newNode;
-
-}
-
-
-public void addLast(int data){
-    Node newNode = new Node(data);
-
-     if (head==null) {
-    head= newNode;
-    return;
-}
-    Node currNode = head;
-    while (currNode.next!=null) {
-        currNode=currNode.next;
-        
-    }
-currNode.next = newNode;
-
-
-}
-
-
-//size
-public int getSize(){
-    return size;
-}
-//print
-public void printList(){
-if (head==null) {
-    System.out.println("List is empty");
-    return;
-}
-    Node currNode = head;
-    while (currNode!=null) {
-        System.out.print(currNode.data+ " -> " );
-        currNode=currNode.next;
-        
+            this.next = null;
+            size = size + 1;
+        }
     }
 
-    System.out.println("NULL");
-}
-
-
-    //remove nth node from end
-public Node removeNthFromEnd(Node head, int n){
-    if (head.next==null) {
-        return null;
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
     }
-int size=0;
-Node curr = head;
-while (curr!=null) {
-    curr=curr.next;
-    size++;
-    
-}
 
-if (n==size) {
-    return head.next;
-}
-int indexToSearch= size-n;
-Node prev=head;
-int i =0;
-while (i<indexToSearch) {
-    prev=prev.next;
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
 
-}
+    public int getSize() {
+        return size;
+    }
 
-prev.next= prev.next.next;
-return head;
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
 
-}
+    public Node removeNthFromEnd(Node head, int n) {
+        if (head.next == null) {
+            return null;
+        }
+        int size = 0;
+        Node curr = head;
+        while (curr != null) {
+            curr = curr.next;
+            size++;
+        }
+        if (n == size) {
+            return head.next;
+        }
+        int indexToSearch = size - n;
+        Node prev = head;
+        int i = 1;
+        while (i < indexToSearch) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return head;
+    }
 
+    public Node reverse(Node head) {
+        Node prev = null;
+        Node curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public Node findMiddle(Node head) {
+        Node hare = head;
+        Node turtle = head;
+        while (hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+            turtle = turtle.next;
+        }
+        return turtle;
+    }
+
+    public boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        Node middle = findMiddle(head);
+        Node secondHalfStart = reverse(middle.next);
+        Node firstHalfStart = head;
+        while (secondHalfStart != null) {
+            if (firstHalfStart.data != secondHalfStart.data) {
+                return false;
+            }
+            firstHalfStart = firstHalfStart.next;
+            secondHalfStart = secondHalfStart.next;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         PracticeLL list = new PracticeLL();
-
         list.addFirst(1);
         list.addFirst(2);
         list.addFirst(3);
-list.addFirst(4);
-list.addFirst(5);
-
-        
-removeNthFromEnd(list,4);
-
-
+        list.addFirst(4);
+        list.addFirst(5);
+        list.head = list.removeNthFromEnd(list.head, 4);
+        list.printList();
     }
 }
